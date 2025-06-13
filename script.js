80c5478e02e26df3cd652b65df179b240b707389
Chikwa235 <chisapachikwa25@gmail.com>
2025-06-13 23:14:02 +0200

    Add createElement helper to simplify element creation and reduce repetition

---
+++ script.js
@@ -0,0 +1,6 @@
+function createElement(tag, text = '', className = '') {
+  const el = document.createElement(tag);
+  if (text) el.textContent = text;
+  if (className) el.className = className;
+  return el;
+}
